db21b38bc05c7511bd68c6f723e169823edc904b
Jinyoung Kim <ligeek49@gmail.com>
2015-04-06 23:28:37 +0900

    randomize vocas in stage
    Vocas randomize to use Fisher-Yates Shuffle Algorithm

--- client/app/stage/stage.controller.js
+++ client/app/stage/stage.controller.js
@@ -1,15 +1,28 @@
 'use strict';
 
 angular.module('vesselApp').controller('StageCtrl', function ($scope, $http, socket) {
-  $scope.wordsInVessel = [];
+  $scope.stageVoca = [];
+
+  // Fisher–Yates shuffle algorithm
+  var shuffleArray = function(array) {
+    var tmp, i;
+
+    for(var m = array.length - 1; m > 0; m--) {
+      i = Math.floor(Math.random() * m);
+
+      tmp = array[m];
+      array[m] = array[i];
+      array[i] = tmp;
+    }
+
+    return array;
+  }
 
   $http.get('/api/vocas').success(function(wordsInVessel) {
-    $scope.wordsInVessel = wordsInVessel;
-    socket.syncUpdates('voca', $scope.wordsInVessel);
+    $scope.stageVoca = shuffleArray(_.clone(wordsInVessel));
   });
 
   $scope.$on('$destroy', function () {
-    socket.unsyncUpdates('voca');
   });
 });
 
